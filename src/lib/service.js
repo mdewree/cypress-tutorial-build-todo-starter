b95db9f9d264b0fb214b4b0abeebd4860d7e790a
Michael De Wree <michael.dewree@ordina.be>
2019-01-08 15:36:31 +0100

    add update func

--- src/lib/service.js
+++ src/lib/service.js
@@ -8,3 +8,6 @@ export const loadTodos = () =>
 
 export const deleteTodo = (id) =>
     axios.delete(`http://localhost:3030/api/todos/${id}`)
+
+export const updateTodo = (id) =>
+    axios.patch(`http://localhost:3030/api/todos/${id}`)
