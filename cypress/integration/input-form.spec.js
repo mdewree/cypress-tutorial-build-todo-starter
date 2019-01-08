8b70490572893f6dfa9c9289119f8f58c01d211c
Michael De Wree <michael.dewree@ordina.be>
2019-01-08 10:50:08 +0100

    add second testfile app-init

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -15,9 +15,12 @@ describe('Input Form', () => {
     })
 
     context('Form Submission', () => {
-        it.only('adds a new todo on submit', () => {
-            const itemText = 'Buy Eggs';
+        beforeEach(() => {
             cy.server();
+        })
+
+        it('adds a new todo on submit', () => {
+            const itemText = 'Buy Eggs';
             cy.route('POST', '/api/todos', {
                 name: itemText,
                 id: 1,
@@ -30,6 +33,22 @@ describe('Input Form', () => {
             cy.get('.todo-list li')
                 .should('have.length', 1)
                 .and('contain', itemText)
+        })
+
+        it('shows an error on a failed submission', () => {
+            cy.route({
+                url: '/api/todos',
+                method: 'POST',
+                status: 500,
+                response: {}
+            });
+            cy.get('.new-todo')
+                .type('Test{enter}');
+            cy.get('.todo-list li')
+                .should('not.exist');
+            cy.get('.error')
+                .should('be.visible');
+
         })
     })
 })
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -29,7 +29,8 @@ export default class TodoApp extends Component {
       .then(({ data }) => this.setState({
         todos: this.state.todos.concat(data),
         currentTodo: ''
-      }));
+      }))
+      .catch(() => this.setState({ error: true }));
   }
 
 
@@ -39,6 +40,7 @@ export default class TodoApp extends Component {
         <div>
           <header className="header">
             <h1>todos</h1>
+            {this.state.error ? <span className='error'>Oh no!</span> : null}
             <TodoForm currentTodo={this.state.currentTodo}
               handleNewTodoChange={this.handleNewTodoChange}
               handleTodoSubmit={this.handleTodoSubmit} />
